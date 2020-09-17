0f2bef4f6edc41ee969628ea3ca05cadf30c9769
Muhamad Fikri Mubarok <40727135+Fikrifire19@users.noreply.github.com>
2020-09-17 09:02:22 +0700

    menambah scss, membuat component header dan page home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "./components/Header/index";
+import Homepage from "./pages/homepage/Homepage";
+import "./styles.scss";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Homepage />
+    </div>
+  );
 }
---
+++ src/components/Directory/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Directory = (props) => {
+  return (
+  <div></div>;
+  );
+};
+
+export default Directory;
