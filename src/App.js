8892aa5ce3e06ecec631ac3cb1dd49f7fe1960ee
Muhamad Fikri Mubarok <40727135+Fikrifire19@users.noreply.github.com>
2020-09-17 16:51:25 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <Homepage />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,8 +1,30 @@
 import React from "react";
+import "./styles.scss";
 
 const Directory = (props) => {
   return (
-  <div></div>;
+    <div className="directory">
+      <div className="wrap">
+        <div
+          className="item"
+          style={{
+            backgroundImage:
+              "url{https://www.marketingmag.com.au/wp-content/uploads/2015/04/young-women-shopping.jpg}"
+          }}
+        >
+          <a>Shop Womens</a>
+        </div>
+        <div
+          className="item"
+          style={{
+            backgroundImage:
+              "url{https://i1.wp.com/nypost.com/wp-content/uploads/sites/2/2018/06/men-shopping-masculine.jpg?quality=80&strip=all&ssl=1}"
+          }}
+        >
+          <a>Shop Mens</a>
+        </div>
+      </div>
+    </div>
   );
 };
 
